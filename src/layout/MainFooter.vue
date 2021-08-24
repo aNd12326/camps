3262ee9698ae906b558bf9046946ae6ff9036bbe
AnD <andersonventosilla0@gmail.com>
2021-08-23 22:45:02 -0500

    kub

---
+++ src/layout/MainFooter.vue
@@ -0,0 +1,80 @@
+<template>
+  <footer
+    class="footer"
+    :class="{ [`footer-${type}`]: type }"
+    :data-background-color="backgroundColor"
+  >
+    <div class="container">
+      <nav>
+        <ul>
+           <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Like us on Facebook"
+          data-placement="bottom"
+          href="https://www.facebook.com/CreativeTim"
+          target="_blank"
+        >
+          <i class="fab fa-facebook-square fa-2x"></i>
+          <p class="d-lg-none d-xl-none">Facebook</p>
+        </a>
+      </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              rel="tooltip"
+              title="Follow us on Twitter"
+              data-placement="bottom"
+              href="https://twitter.com/CreativeTim"
+              target="_blank"
+            >
+              <i class="fab fa-twitter fa-2x"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Instagram"
+          data-placement="bottom"
+          href="https://www.instagram.com/CreativeTimOfficial"
+          target="_blank"
+        >
+          <i class="fab fa-instagram fa-2x"></i>
+          <p class="d-lg-none d-xl-none">Instagram</p>
+        </a>
+      </li>
+        </ul>
+      </nav>
+      <div class="copyright">
+        &copy; {{ year }}, Designed by
+        <a href="https:///www.invisionapp.com" target="_blank" rel="noopener"
+          >Invision</a
+        >. Coded by
+        <a href="https://binarcode.com" target="_blank" rel="noopener"
+          >BinarCode</a
+        >
+        and
+        <a href="https://www.creative-tim.com" target="_blank" rel="noopener"
+          >Creative Tim</a
+        >.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    backgroundColor: String,
+    type: String,
+  },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+<style></style>
---
+++ src/layout/MainNavbar.vue
@@ -0,0 +1,93 @@
+<template>
+  <navbar
+    position="fixed"
+    type="primary"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template>
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        <div id="font">
+          FM Camps
+        </div>
+      </router-link>
+      <el-popover
+        ref="popover1"
+        popper-class="popover"
+        placement="bottom"
+        width="200"
+        trigger="hover"
+      >
+        <!-- <div class="popover-body">
+          Designed by Invision. Coded by Creative Tim
+        </div> -->
+      </el-popover>
+    </template>
+    <template slot="navbar-menu">
+      <li class="nav-item">
+        <a
+          class="nav-link "
+          to="/"
+          target="_blank"
+        >
+          <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+          <p>Inicio</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="/landing"
+          target="_blank"
+        >
+          <!-- <i class="now-ui-icons arrows-1_cloud-download-93"></i> -->
+          <p>Nosotros</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="/landing"
+          target="_blank"
+        >
+          <!-- <i class="now-ui-icons arrows-1_cloud-download-93"></i> -->
+          <p>Servicios</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+            
+          target="_blank"
+        >
+          <!-- <i class="now-ui-icons arrows-1_cloud-download-93"></i> -->
+          <p>Contacto</p>
+        </a>
+      </li>
+
+
+    </template>
+  </navbar>
+</template>
+
+<script>
+import { Navbar } from '@/components';
+import { Popover } from 'element-ui';
+export default {
+  name: 'main-navbar',
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    Navbar,
+    [Popover.name]: Popover
+  }
+};
+</script>
+
+<style scoped></style>
